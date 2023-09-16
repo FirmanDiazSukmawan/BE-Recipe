d107a38548f730788c76a470268a9f5ba35faf2e
xTats <firman670676@gmail.com>
2023-09-16 19:25:38 +0700

    update cloudinary

--- index.js
+++ index.js
@@ -6,26 +6,22 @@ const helmet = require("helmet");
 const xssclean = require("xss-clean");
 const mainRouter = require("./src/router/mainRouter");
 
-
-
 const PORT = 3002;
 app.use(cors());
 app.use(helmet());
 app.use(xssclean());
 app.use(bodyParser.json());
 app.use(mainRouter);
-
-
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "server ready to use",
-    });
+  res.status(200).json({
+    message: "server ready to use",
+  });
 });
 
-
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
 
 // syntax menggunakan query & params
@@ -73,4 +69,3 @@ app.listen(PORT, () => {
 //         success: "data terkirim",
 //     });
 // });
-
